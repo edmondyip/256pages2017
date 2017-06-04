25dc4ca21b68a12c3116b55857226fd1dfda1789
EdmondYip <neta.edmond@gmail.com>
2017-06-04 16:18:12 +0800

    UPDATE

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,7 @@
 <template>
   <section>
       <p>I am Edmond. I am a UX/UI Designer in Hong Kong. I also work in web front-end. This website is using full Javascript to development. Available freelance jobs. Contact me if you need someone working at front-end.</p>
+      <div></div>
   </section>
 </template>
 

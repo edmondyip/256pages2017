cca462dd05ef98aeeda682e7d45ff9db05310363
Edmond <edmond.yip@glassesgallery.com>
2017-07-12 11:02:52 +0800

    update

--- layouts/index.vue
+++ layouts/index.vue
@@ -1,32 +1,13 @@
 <!-- Template -->
 <template>
-<<<<<<< HEAD
-      <nuxt/>
-  <app-footer/>
-=======
   <div id="container">
     <app-navbar/>
     <nuxt/>
   </div>
->>>>>>> b66a1517654f9e9e2d458c4649dc213099a20c81
 </template>
 
 <!-- Script -->
 <script>
-<<<<<<< HEAD
-  import Header from '~components/pages-header.vue'
-  import Footer from '~components/pages-footer.vue'
-  export default {
-    name: "DefaultLayout",
-    components: {
-      "app-header" : Header,
-      "app-footer" : Footer
-    },
-    data: () => ({
-      tab: false
-    })
-  }
-=======
 import NavBar from '~components/index-nav.vue'
 export default {
   name: "IndexLayout",
@@ -34,7 +15,6 @@ export default {
     "app-navbar" : NavBar
   }
 }
->>>>>>> b66a1517654f9e9e2d458c4649dc213099a20c81
 </script>
 
 <!-- Style -->
@@ -43,25 +23,10 @@ export default {
   #container {
     flex-wrap: nowrap;
     width: var(--container-width);
-<<<<<<< HEAD
-    height: 100%;
-=======
->>>>>>> b66a1517654f9e9e2d458c4649dc213099a20c81
     margin-left: -10rem;
     opacity: 1;
     transition: .5s;
     background: #fff;
-<<<<<<< HEAD
-    #content {
-      padding: 1rem;
-      width: 100%;
-      position: relative;
-      align-content: flex-start;
-      transition: .5s;
-      z-index: 100;
-    }
-=======
->>>>>>> b66a1517654f9e9e2d458c4649dc213099a20c81
   }
   #container.active {
     margin-left: 0;
@@ -96,29 +61,9 @@ export default {
   @include for-desktop {
     #container {
       width: 1200px;
-<<<<<<< HEAD
-      height: 95vh;
-      margin: 2.5vh auto;
-      box-shadow: 0 0 5rem rgba(0,0,0,.2);
-    }
-  }
-  @include for-desktop {
-    #container {
-      width: 1200px;
-      height: 95vh;
-=======
       height: 80vh;
->>>>>>> b66a1517654f9e9e2d458c4649dc213099a20c81
       margin: 2.5vh auto;
       box-shadow: 0 0 5rem rgba(0,0,0,.2);
     }
   }
 </style>
-<<<<<<< HEAD
-
-
-<!--
-
--->
-=======
->>>>>>> b66a1517654f9e9e2d458c4649dc213099a20c81

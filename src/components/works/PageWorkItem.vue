08bf07c427bbdf3dd19f91d15ea243f0a5f77362
Edmond Yip <neta.edmond@gmail.com>
2017-11-14 01:31:55 +0800

    update works

--- src/components/works/PageWorkItem.vue
+++ src/components/works/PageWorkItem.vue
@@ -2,7 +2,12 @@
   <isotope ref="grid" :options='option' :list="list" :item-selector="'element-item'" v-images-loaded:on.progress="layout" class="works-list">
     <section v-for="(work, index) in list" :key="index" :class="{'checked': index === projectView}" @click="changeProjectView(index)">
       <div class="wrapper">
-        <img :src="require('../../assets/img/works/' + work.path + '/' + work.gallery[0].fileName)" :alt="work.name + work.gallery[0].title">
+        <carousel :perPage="1" v-if="index === projectView">
+          <slide v-for="(image,index) in work.gallery" :key="index">
+            <img :src="require('../../assets/img/works/' + work.path + '/' + image.fileName)" :alt="work.name + image.title">
+          </slide>
+        </carousel>
+        <img v-else :src="require('../../assets/img/works/' + work.path + '/' + work.gallery[0].fileName)" :alt="work.name + work.gallery[0].title">
         <div class="description">
           <h2>{{ work.name }}</h2>
           <p>{{ work.description }}</p>
@@ -19,6 +24,7 @@
   import worksList from '@/assets/json/projects.json'
   import isotope from 'vueisotope'
   import imagesLoaded from 'vue-images-loaded'
+  import {Carousel, Slide} from 'vue-carousel'
   export default {
     name: 'WorksItem',
     data: function () {
@@ -52,7 +58,9 @@
       }
     },
     components: {
-      isotope
+      isotope,
+      Carousel,
+      Slide
     },
     directives: {
       imagesLoaded
@@ -61,19 +69,6 @@
 </script>
 
 <style scoped lang="scss">
-  // .filter-button-group {
-  //   margin: 0 auto;
-  //   display: flex;
-  //   width: 200px;
-  //   justify-content: space-around;
-  // }
-  // .filter-button-group button {
-  //   background: #4A4A4A;
-  //   color: #85F1C1;
-  //   font-family: 'Bitter', sans-serif;
-  //   border: 0;
-  //   padding: 10px;
-  // }
   .works-list {
     width: 980px;
     margin: 0 auto;
@@ -99,7 +94,7 @@
         width: 100%;
         display: block;
       }
-      ul {
+      .description {
         position: absolute;
         display: block;
         padding: 0;
@@ -111,6 +106,9 @@
         right: -100%;
         bottom: 5px;
         list-style: none;
+        p {
+          display: none;
+        }
         li {
           background: #85F1C1;
           font-size: 12px;
@@ -119,86 +117,76 @@
           display: inline-block;
           padding: 8px;
         }
+        li:first-child {
+          margin: 5px 5px 5px 0;
+        }
       }
+    }
+    .VueCarousel {
+      background: #7ADDC9;
+      margin: 0;
+      padding: 20px;
     }
   }
 
-  .works-list section ul>li:first-child {
-    margin: 5px 5px 5px 0;
+  .works-list section:hover {
+    h2 {
+      left: 0px;
+    }
+    ul.description {
+      right: 5px;
+    }
   }
-
-  .works-list section:hover h2 {
-    left: 0px;
-  }
-
-  .works-list section:hover ul {
-    right: 5px;
-  }
-
-  .works-list section .description {
-    background: none;
-    padding: 0 20px;
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(255, 255, 255, 0);
-    transition: 1s;
-  }
-
-  .works-list section .description p {
-    display: none;
-  }
-
-  .works-list img {
-    list-style: none;
-    overflow: hidden;
-    background: #7ADDC9;
-    margin: 0;
-    padding: 0;
-  }
+  
   /* checked */
 
-  .works-list .checked {
+  .works-list section.checked {
     width: 100%;
     background: rgba(255, 255, 255, 1);
-  }
-
-  .works-list .checked .wrapper {
-    position: relative;
-    display: block;
-  }
-
-  .works-list .checked .description {
-    display: inline-block;
-    position: relative;
-    width: 40%;
-    background: rgba(255, 255, 255, 1);
-    height: 100%;
-    left: 0px;
-    top: -50px;
-    bottom: 0px;
-  }
-
-  .works-list .checked .description p,
-  .works-list .checked .description h2 {
-    display: block;
-  }
-
-  .works-list .checked img {
-    display: inline-block;
-    height: 100%;
-    width: 50%;
-  }
-
-  .works-list .checked h2,
-  .works-list .checked ul {
-    position: relative;
-    width: auto;
-    left: unset!important;
-    right: unset!important;
-  }
+    .wrapper {
+      position: relative;
+      display: block;
+    }
+    .description {
+      display: inline-block;
+      position: relative;
+      width: 40%;
+      background: rgba(255, 255, 255, 1);
+      height: 100%;
+      left: 0px;
+      top: -50px;
+      bottom: 0px;
+      h2,
+      p {
+        display: block;
+      }
+      h2,
+      ul {
+        position: relative;
+        width: auto;
+        left: unset!important;
+        right: unset!important;
+      }
+    }
+    .VueCarousel {
+      display: inline-block;
+      height: 100%;
+      width: 50%;
+    }
+  } 
+  
+  // .filter-button-group {
+  //   margin: 0 auto;
+  //   display: flex;
+  //   width: 200px;
+  //   justify-content: space-around;
+  // }
+  // .filter-button-group button {
+  //   background: #4A4A4A;
+  //   color: #85F1C1;
+  //   font-family: 'Bitter', sans-serif;
+  //   border: 0;
+  //   padding: 10px;
+  // }
 
 </style>

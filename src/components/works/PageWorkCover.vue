ecc881c629183a3f3bd1f60967abb35ee2cf782d
Edmond <edmond.yip@glassesgallery.com>
2017-11-14 19:04:03 +0800

    update components

---
+++ src/components/works/PageWorkCover.vue
@@ -0,0 +1,17 @@
+<template>
+  <img :src="require('../../assets/img/works/' + path + '/' + image.fileName)" :alt="name + image.title">
+</template>
+
+<script>
+  export default {
+    props: ['image','path','name']
+  }
+</script>
+
+<style scoped lang="scss">
+      img {
+        width: 100%;
+        display: block;
+        cursor: pointer;
+      }
+</style>
--- src/components/works/PageWorkItem.vue
+++ src/components/works/PageWorkItem.vue
@@ -1,30 +1,39 @@
 <template>
   <isotope ref="grid" :options='option' :list="list" :item-selector="'element-item'" v-images-loaded:on.progress="layout" class="works-list">
     <section v-for="(work, index) in list" :key="index" :class="{'checked': index === projectView}" @click="changeProjectView(index)">
-      <div class="wrapper">
-        <carousel :perPage="1" v-if="index === projectView">
-          <slide v-for="(image,index) in work.gallery" :key="index">
-            <img :src="require('../../assets/img/works/' + work.path + '/' + image.fileName)" :alt="work.name + image.title">
-          </slide>
-        </carousel>
-        <img v-else :src="require('../../assets/img/works/' + work.path + '/' + work.gallery[0].fileName)" :alt="work.name + work.gallery[0].title">
-        <div class="description">
-          <h2>{{ work.name }}</h2>
-          <p>{{ work.description }}</p>
-          <ul>
-            <li v-for="type in work.typeList" :key="type.id">{{ type }}</li>
-          </ul>
+      <transition>
+        <div class="description" v-if="index === projectView">
+          <carousel :perPage="1">
+            <slide v-for="(image,index) in work.gallery" :key="index">
+              <work-cover :image="image" :path="work.path" :name="work.name"></work-cover>
+            </slide>
+          </carousel>
+          <div class="info">
+            <div class="close" @click="index = null">close</div>
+            <h2>{{ work.name }}</h2>
+            <p>{{ work.description }}</p>
+            <work-type :typeList="work.typeList"></work-type>
+          </div>
         </div>
-      </div>
+        <div v-else class="cover">
+          <work-cover :image="work.gallery[0]" :path="work.path" :name="work.name"></work-cover>
+          <work-type :typeList="work.typeList"></work-type>
+        </div>
+      </transition>
     </section>
   </isotope>
 </template>
 
 <script>
   import worksList from '@/assets/json/projects.json'
+  import workType from '@/components/works/PageWorkType.vue'
+  import workCover from '@/components/works/PageWorkCover.vue'
   import isotope from 'vueisotope'
   import imagesLoaded from 'vue-images-loaded'
-  import {Carousel, Slide} from 'vue-carousel'
+  import {
+    Carousel,
+    Slide
+  } from 'vue-carousel'
   export default {
     name: 'WorksItem',
     data: function () {
@@ -36,31 +45,36 @@
           stagger: 30,
           masonry: {
             fitwidth: true,
-            gutter: 20
+            gutter: 20,
+            columnWidth: 50
           }
         }
       }
     },
     methods: {
       changeProjectView: function (index) {
+        var imgHeight = ('../../assets/img/works/' + this.list.path + '/' + this.list.gallery.fileName).clientHeight
+        console.log(imgHeight)
         if (index === this.projectView) {
-          this.projectView = null
+          // this.projectView = null
         } else {
           this.projectView = index
         }
         var self = this
         setTimeout(function () {
           self.$refs.grid.iso.layout()
-        }, 400)
+        }, 550)
       },
-      layout () {
+      layout() {
         this.$refs.grid.layout('masonry')
       }
     },
     components: {
       isotope,
       Carousel,
-      Slide
+      Slide,
+      workType,
+      workCover
     },
     directives: {
       imagesLoaded
@@ -77,105 +91,79 @@
       position: relative;
       overflow: hidden;
       display: block;
-      width: 22%;
-      cursor: pointer;
+      width: 24%;
       transition: width .5s, height .5s;
       h2 {
         background: #4A4A4A;
         color: #fff;
         line-height: 30px;
         height: 30px;
-        left: -200px;
-        transition: .5s;
-        padding: 0 20px;
-        display: none;
+        padding: 0 10px;
+        margin: 0;
       }
       img {
         width: 100%;
         display: block;
+        cursor: pointer;
       }
       .description {
-        position: absolute;
         display: block;
         padding: 0;
         margin: 0;
         text-decoration: none;
-        -webkit-transition-delay: .5s;
-        transition-delay: .5s;
         transition: .5s;
-        right: -100%;
-        bottom: 5px;
-        list-style: none;
-        p {
-          display: none;
-        }
-        li {
-          background: #85F1C1;
-          font-size: 12px;
-          line-height: 14px;
-          margin: 5px;
-          display: inline-block;
-          padding: 8px;
-        }
-        li:first-child {
-          margin: 5px 5px 5px 0;
+      }
+    }
+    .cover /deep/ ul.type {
+      position: absolute;
+    }
+    .close {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      font-size: 12px;
+      color: #cccccc;
+      cursor: pointer;
+    }
+    section:hover {
+      h2 {
+        left: 0px;
+      }
+      .description .cover {
+        /deep/ ul.type {
+          right: 5px;
         }
       }
     }
+  }
+  /* checked */
+
+  .works-list section.checked {
+    width: 980px;
+    height: 500px;
+    background: rgba(255, 255, 255, 1);
+    .info {
+      display: inline-block;
+      position: relative;
+      width: 35%;
+      background: rgba(255, 255, 255, 1);
+      height: 100%;
+      vertical-align: top;
+      /deep/ ul.type {
+        position: relative;
+        right: unset;
+      }
+    }
     .VueCarousel {
+      display: inline-block;
+      overflow: hidden;
+      height: 50%;
+      width: 60%;
       background: #7ADDC9;
       margin: 0;
       padding: 20px;
     }
-  }
-
-  .works-list section:hover {
-    h2 {
-      left: 0px;
-    }
-    ul.description {
-      right: 5px;
-    }
-  }
-  
-  /* checked */
-
-  .works-list section.checked {
-    width: 100%;
-    background: rgba(255, 255, 255, 1);
-    .wrapper {
-      position: relative;
-      display: block;
-    }
-    .description {
-      display: inline-block;
-      position: relative;
-      width: 40%;
-      background: rgba(255, 255, 255, 1);
-      height: 100%;
-      left: 0px;
-      top: -50px;
-      bottom: 0px;
-      h2,
-      p {
-        display: block;
-      }
-      h2,
-      ul {
-        position: relative;
-        width: auto;
-        left: unset!important;
-        right: unset!important;
-      }
-    }
-    .VueCarousel {
-      display: inline-block;
-      height: 100%;
-      width: 50%;
-    }
-  } 
-  
-  // .filter-button-group {
+  } // .filter-button-group {
   //   margin: 0 auto;
   //   display: flex;
   //   width: 200px;
